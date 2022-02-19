e87e9b840095f8e17a94a03ff88f245f0f7eb5d2
keelcoutinho <kelvia_cou@hotmail.com>
2022-02-19 19:19:52 -0300

    modificação 1

--- Exemplo3.java
+++ Exemplo3.java
@@ -20,8 +20,10 @@ a3 = leia.nextFloat();
 System.out.println("\n Entre com o quarto valor: ");
 a4 = leia.nextFloat();
 
+
 if (Math.pow(a3,2)>= 1000) {
-    System.out.println("\n O quadrado do terceiro valor é: " + Math.pow(a3,2));
+    
+    System.out.println("\n O terceiro valor é "+a3+" e seu quadado é: "+ Math.pow(a3,2));
 }
 else {
     System.out.println("O valor é: "+ a1+ ". O quadrado é: "+ Math.pow(a1,2));
@@ -29,3 +31,5 @@ else {
     System.out.println("O valor é: "+ a3+ ". O quadrado é: "+ Math.pow(a3,2));
     System.out.println("O valor é: "+ a4+ ". O quadrado é: "+ Math.pow(a4,2));
 }
+
+    
